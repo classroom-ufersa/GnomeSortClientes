97ffe001e65a627680eaa2e17f998cb4bd30f35c
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2024-02-07 09:54:01 -0300

    feat: I added the library and the function

--- cliente.c
+++ cliente.c
@@ -1,6 +1,17 @@
+#include "cliente.h"
+#include <stdio.h>
+#include <stdlib.h>
 struct cliente{
     char nome[50];
     char endereco[100];
     int cod_cliente;
 };
+
+void PreEncherCliente(struct cliente *cliente){
+    printf("Nome do cliente: ");
+    scanf(" [^\n]", cliente->nome);
+
+    printf("Endereco: ");
+    scanf(" [^\n]", cliente->endereco);
+}
 
