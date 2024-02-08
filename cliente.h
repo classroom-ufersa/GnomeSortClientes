c8a167a8ef230e112ff235025ebbbf0ce2eaf8f9
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2024-02-08 10:33:47 -0300

    feat: adição de funcionalidades

---
+++ cliente.h
@@ -0,0 +1,8 @@
+// estrutura de dados para o endereco do cliente
+struct Endereco;
+
+// estrutura de dados para as informações do cliente
+struct cliente;
+
+// função que recebe um ponteiro para a estrutura do cliente e preenche os dados
+void PreEncherCliente(struct cliente *cliente);
