87c763db2238b2af3251103bc11b949be9dab6fe
fabricio-dev-front <139814025+fabricio-dev-front@users.noreply.github.com>
2024-02-17 16:17:17 -0300

    feat: mudanças realizadas

--- cliente.c
+++ cliente.c
@@ -1,23 +1,38 @@
 #include "cliente.h"
-#include <stdio.h>
-#include <stdlib.h>
 
-// Estrutura do endereco do cliente
-struct Endereco{
-    char nome_da_rua[20];
-    char barirro[20];
-    char cidade[20];
-    int numero_da_casa; 
-    int CEP;
-};
+// Função para carregar clientes a partir de um arquivo
+int carregarClientes(Cliente **clientes){
+    // Abre o arquivo "clientes_ordenados.txt" em modo de leitura
+    FILE *arquivo = fopen("clientes_ordenados.txt", "r");
+    
+    // Verifica se a abertura do arquivo foi bem-sucedida
+    if(arquivo == NULL){
+        perror("Erro ao abrir o arquivo");
+        return 0;
+    }
 
-// Estrutura do cliente
-struct cliente{
-    char nome[50];
-    int cod_cliente;
+    int quantidade;
+    
+    // Lê a quantidade de clientes do arquivo
+    fscanf(arquivo, "%d", &quantidade);
 
-    struct Endereco endereco;
-};
+    // Aloca dinamicamente memória para armazenar os clientes
+    *clientes = (Cliente *)malloc(quantidade * sizeof(Cliente));
+    
+    // Loop para ler dados de cada cliente do arquivo
+    for(int i = 0; i < quantidade; i++){
+        // Lê código, nome e endereço de cada cliente e armazena na estrutura
+        fscanf(arquivo, "%s %s %[^\n]", (*clientes)[i].codigo, (*clientes)[i].nome, (*clientes)[i].endereco);
+    }
+
+    // Fecha o arquivo após a leitura
+    fclose(arquivo);
+
+    // Retorna a quantidade total de clientes lidos
+    return quantidade;
+}
+
+
 
 // Função para preencher os dados do cliente
 void PreEncherCliente(struct cliente *cliente){
