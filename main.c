78fc2189ba701a1c7d311c6e26b9115faaf90684
Gustavo Kesley Fontes <72459288+gustavo-f0ntz@users.noreply.github.com>
2024-02-19 12:58:52 -0300

    feat: solicitando a quantidade de clientes a serem cadastrados

--- main.c
+++ main.c
@@ -5,5 +5,14 @@ int main() {
     double cpu_time_used;
 
     start = clock();
-    end = clock();
-}
+
+    int quantidade_existente = 0;
+    Cliente *clientes = NULL;
+
+    quantidade_existente = carregarClientes(&clientes);
+
+    int quantidade_nova;
+    printf("Informe a quantidade de novos clientes a serem cadastrados: ");
+    scanf("%d", &quantidade_nova);
+
+    
