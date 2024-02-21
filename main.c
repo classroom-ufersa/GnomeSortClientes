1058385de44338bc29b96a1e63e0724a01b61098
Dinarte Filho <dinarte_filho@hotmail.com>
2024-02-21 14:31:22 -0300

    Reajustando o clock para a função de ordenação

--- main.c
+++ main.c
@@ -1,10 +1,8 @@
 #include "cliente.h"
 
 int main() {
-    clock_t start, end;
-    double cpu_time_used;
-
-    start = clock();
+    clock_t inicio, fim;
+    double tempo_de_execucao;
 
     int quantidade_existente = 0;
     Cliente *clientes = NULL;
@@ -16,18 +14,15 @@ int main() {
     scanf("%d", &quantidade_nova);
 
     adicionarClientes(&clientes, &quantidade_existente, quantidade_nova);
+    inicio = clock();
     gnomeSort(clientes, quantidade_existente);
+    fim = clock();
     salvarClientes(clientes, quantidade_existente);
 
     free(clientes);
 
-    end = clock();
-    cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
-
-    printf("Tempo de execucao: %f segundos\n", cpu_time_used);
+    tempo_de_execucao = ((double) (fim - inicio)) / CLOCKS_PER_SEC;
+    printf("Tempo de execucao: %f segundos\n", tempo_de_execucao);
 
     return 0;
-} 
-
-
-    
+}
